90201f7f87a3b12130a65733bb0c727a5103abf2
amenir88 <ameni.rommene@esprit.tn>
2026-02-19 10:14:49 +0100

    coorection de syntaxe

--- src/app/features/suggestions/suggestion-form/suggestion-form.component.ts
+++ src/app/features/suggestions/suggestion-form/suggestion-form.component.ts
@@ -8,12 +8,26 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class SuggestionFormComponent implements OnInit {
 myForm! : FormGroup;
+id : string = '1';
+categories: string[] =
+[ 'Infrastructure et bâtiments',
+  'Technologie et services numériques',
+   'Restauration et cafétéria',
+   'Hygiène et environnement',
+   'Transport et mobilité',
+   'Activités et événements',
+   'Sécurité', 'Communication interne', 'Accessibilité', 'Autre' ];
 
+    onSubmit(){
+
+  }
 ngOnInit(){
 this.myForm= new FormGroup({
   title : new FormControl("",Validators.required),
   description: new FormControl("",[Validators.required,Validators.minLength(3)])
 })
   }
+
+
 
 }
